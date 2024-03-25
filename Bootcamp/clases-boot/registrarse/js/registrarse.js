31aa2c617d45660a8b3b2968d1f0ca3775815844
srlemusing <jhonatanlp0808.jl@gmail.com>
2024-03-25 01:28:35 -0500

    v.1.11

---
+++ Bootcamp/clases-boot/registrarse/js/registrarse.js
@@ -0,0 +1,8 @@
+
+//Damos inicio a nuestra interaccion con JavaScript.
+
+    var capturar = function()  //creamos la variable capturar anexo a la declararla como funcion.
+    { 
+        var nom = document.getElementById("minombre").value //creamos la variable nom para relacionar el id minombre para guardar el nombrel del usuario
+        console.log(nom)
+    }
