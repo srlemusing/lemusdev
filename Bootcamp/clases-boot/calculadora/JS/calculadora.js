936c8a562c866d989e0000c0c0d6ba366ed6adfb
srlemusing <jhonatanlp0808.jl@gmail.com>
2024-03-26 23:11:06 -0500

    v.1.13

--- Bootcamp/clases-boot/calculadora/JS/calculadora.js
+++ Bootcamp/clases-boot/calculadora/JS/calculadora.js
@@ -1,9 +1,11 @@
+//En este archivo estamos definiendo las funcionalidades especificas
+//Para llevar a cabo la captura de datos
+//Definimos una funcion por cada operacion: sumar, restar, multiplicar y dividir
 
-
-var sumar = function() {
-    var num1 = document.getElementById("numero1").value
-    var num2 = document.getElementById("numero2").value
-    var numA = parseInt(num1)
+var sumar = function() { 
+    var num1 = document.getElementById("numero1").value //Indicamos que a la variable num1 le pase lo insertado en el id numero1 del HTML y lo conserve para la operacion siendo este el primer numero
+    var num2 = document.getElementById("numero2").value //Indicamos que a la variable num2 le pase lo insertado en el id numero2 del HTML y lo conserve para la operacion siendo este el segundo numero
+    var numA = parseInt(num1) //Indicamos parseInt para convertir de texto a numero los datos insertados por el usuario y conservado en la variable num1 y que lo reescriba en la nueva variable numA
     var numB = parseInt(num2)
     alert(numA+numB)
 }
